fbdeba894913b1915db5beb87e56915ec54550d5
haru210 <miroku.pr@outlook.jp>
2023-05-20 09:13:36 +0900

    各桁の和を求める

--- ABS/some_sums.cpp
+++ ABS/some_sums.cpp
@@ -5,6 +5,32 @@ using namespace std;
 
 int main()
 {
-    
+    int n;
+    int a, b;
+    int tmp[5];
+    int num;
+    int sum = 0;
+    cin >> n >> a >> b;
+    rep(i, n + 1)
+    {
+        int j = 1;
+        num = i;
+        tmp[0] = num %10;
+        while(num > 9){
+            num /= 10;
+            tmp[j] = num %10;
+            j++;  
+        }
+        int tmp_sum = 0;
+        for(int k = 0; k < j; k++)
+        {
+            tmp_sum += tmp[k];
+        }
+        if(tmp_sum >= a && tmp_sum <= b)
+        {
+            sum += i;
+        }   
+    }
+    cout << sum << endl;
     return 0;
 }
