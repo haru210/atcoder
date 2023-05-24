a15c12dd4e38085c77e26397609d7751053a44ee
haru210 <miroku.pr@outlook.jp>
2023-05-24 19:35:17 +0900

    next_permutationを用いた並び順の全探索

--- ABC302/C.cpp
+++ ABC302/C.cpp
@@ -5,5 +5,35 @@ using namespace std;
 
 int main()
 {
+    int n, m;
+    int cnt = 0;
+    bool key = false;
+    cin >> n >> m;
+    vector<string> s(n);
+    string T;
+    rep(i, n)
+    {
+        cin >> s[i];
+    }
+    sort(s.begin(), s.end());
+    do
+    {
+        key = true;
+        rep(i, n - 1) 
+        {
+            cnt = 0;
+            rep(j, m)
+            {
+                if(s[i][j] != s[i+1][j]) cnt++;
+            }
+            if(cnt != 1) key = false; 
+        }
+        if(key)
+        {
+            cout << "Yes\n";
+            return 0;
+        }
+    }while(next_permutation(s.begin(),s.end()));
+    cout << "No\n";
     return 0;
 }
