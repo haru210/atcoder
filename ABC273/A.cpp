69e41b6812121727af2a3575a47c2b1ef20f0482
haru210 <miroku.pr@outlook.jp>
2023-07-22 17:40:14 +0900

    順にかけるだけ

---
+++ ABC273/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << " No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << " NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+int main()
+{
+    int n;
+    int sum = 1;
+    cin >> n;
+    for(int i = 2; i <= n; i++)
+    {
+        sum *= i;
+    }
+    cout << sum << endl;
+    return 0;
+}
