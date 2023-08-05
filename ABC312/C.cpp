1879e32d61131f8368dd96923e18e744b6e18f9f
haru210 <miroku.pr@outlook.jp>
2023-08-05 17:39:01 +0900

    二分探索じゃない方の解法

--- ABC312/C.cpp
+++ ABC312/C.cpp
@@ -14,56 +14,34 @@ int min_bound(int a[],int a_size,int num);
 int main()
 {
     int n,m;
-    cin >> n >> m;
-    int a[n],b[m];
-    map<int,int> a_num;
-    rep(i,n)
+    cin >> n>> m;
+    int sell[n],buy[m];
+    pair<int,bool> event[n+m]; //true=売り false=買い
+    rep(i,n) cin >> sell[i];
+    rep(i,m) cin >> buy[i];
+    rep(i,n+m)
     {
-        int tmp;
-        cin >> tmp;
-        a[i] = tmp;
-        a_num[tmp]++;
-    }
-    rep(i,m) cin >> b[i];
-    //売り手の人数が変わるのはa[i]のみ
-    //ソートすれば添え字+1＝人数にできる?
-    sort(a,a+n);
-    sort(b,b+m,greater<int>());
-    int buyer,seller=0;
-    int i = 0;
-    if(b[0] < a[0])
-    {
-        cout << b[0]+1 << endl;
-    }
-    else for(auto [key,val]: a_num)
-    {
-        buyer = min_bound(b,m,110);
-        seller += val;
-        if(seller >= buyer)
+        if(i < n){
+            event[i].first = sell[i];
+            event[i].second = true;
+        }
+        else
         {
-            cout << a[i] << endl;
+            event[i].first = buy[i-n]+1;
+            event[i].second = false;
+        }
+    }
+    sort(event,event+(n+m));
+    int bx=m,sx=0;
+    rep(i,n+m)
+    {
+        if(event[i].second) sx++;
+        if(!event[i].second) bx--;
+        if(sx >= bx)
+        {
+            cout << event[i].first << endl;
             break;
         }
-        i += val;
     }
     return 0;
-}
-// a{10000,120,100,80} num 110 numより小さいを真、大きいを偽、leftは常に偽,rightは常に真
-int min_bound(int a[],int a_size,int num)
-{
-    
-    int left,right;
-    int center;
-    left = -1;
-    right = a_size;
-    while(right - left > 1)
-    {
-        center = left + ((right - left) / 2);
-        if(a[center] > num)
-        {
-            left = center;
-        }
-        else if(a[center] <= num)right = center;
-    }
-    return right;
 }
