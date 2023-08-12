885aad6ddd1210edff92a8b34e4d56d08d18d135
haru210 <miroku.pr@outlook.jp>
2023-08-12 22:57:27 +0900

    そのまま

---
+++ ABC314/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << " No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << " NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+int main()
+{
+    string s;
+    s = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+    int n;
+    cin >> n;
+    rep(i,n+2)
+    {
+        cout << s[i];
+    }
+    cout << endl;
+    return 0;
+}
