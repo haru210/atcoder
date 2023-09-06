2a234d108c21a4402323d01133cfc0cccd3aabdc
haru210 <harun20070320@gmail.com>
2023-09-06 15:49:27 +0900

    Aを100で割ってBかける

---
+++ ABC205/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << " No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << " NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+int main()
+{
+     double a;
+    int b;
+    cin >> a >> b;
+    cout << a / 100.0 * (double)b;
+    return 0;
+}
