eb8835d0a825ef60fb16b19deace0b32f685963e
haru210 <harun20070320@gmail.com>
2023-09-21 22:16:03 +0900

    順番に足し合わせていって超えたらbreakしてcout

---
+++ ABC206/B.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+int main()
+{
+    int n;
+    cin >> n;
+    int day = 0;
+    ll ans = 0;
+    while(ans < n)
+    {
+        day++;
+        ans += day;
+    }
+    cout << day << endl;
+    return 0;
+}
