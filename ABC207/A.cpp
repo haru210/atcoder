d1a3c706c492060f6b58f620e4e6a1bd1aedc716
haru210 <harun20070320@gmail.com>
2023-10-07 20:38:55 +0900

    そのまま

---
+++ ABC207/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+int main()
+{
+    int a[3];
+    cin >> a[0] >> a[1] >> a[2];
+    sort(a,a+3);
+    cout << a[1] + a[2] << endl;
+    return 0;
+}
