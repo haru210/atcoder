1885472e0ddbd58d34388d599b419e49e39aef9b
haru210 <harun20070320@gmail.com>
2023-10-07 20:38:12 +0900

    一つでa-1個増える

---
+++ ABC139/B.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    int ans = 0;
+    int now = 1;
+    while(now < b)
+    {
+        now += a - 1;
+        ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
