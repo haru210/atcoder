463e813cc2ad2019db2950b5f79f7fe0f163992b
haru210 <harun20070320@gmail.com>
2023-11-14 15:20:42 +0900

    dpは1-indexedのほうがいいかも...?

---
+++ EDPC/B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+int main()
+{
+    int n,k;
+    cin >> n >> k;
+    int h[n+1];
+    rep2(i,n) cin >> h[i];
+    int dp[n+1];
+    rep2(i,n) dp[i] = inf;
+    dp[1] = 0;
+    rep2(i, n)
+    {
+        for(int j = 1; j <= min(k, n-i);j++)
+        {
+            dp[i+j] = min(dp[i+j], dp[i] + abs(h[i] - h[i+j]));
+        }
+    }
+    cout << dp[n] << endl;
+    return 0;
+}
