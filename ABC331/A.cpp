bc5d27c5e122f3a031038075ea81d1e6441288df
haru210 <harun20070320@gmail.com>
2023-12-02 22:42:46 +0900

    やるだけ

---
+++ ABC331/A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+int main()
+{
+    int M,D;
+    int y,m,d;
+    cin >> M >> D >> y >> m >> d;
+    if(d == D)
+    {
+        if(m == M)
+        {
+            cout << y + 1 << " 1 1\n";
+        }
+        else
+        {
+            cout << y << ' ' << m + 1 << " 1\n";
+        }
+    }
+    else cout << y << ' ' << m << ' ' << d +1<< endl;
+    return 0;
+}
