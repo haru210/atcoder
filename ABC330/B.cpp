fc1c50139d3032d6209a9517e22d214a4fa98e24
haru210 <harun20070320@gmail.com>
2023-12-02 16:56:22 +0900

    l < ai < rならai、それ以外ならlかr

---
+++ ABC330/B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+int main()
+{
+    int n,l,r;
+    cin >> n >> l >> r;
+    int a[n];
+    rep(i,n) cin >> a[i];
+    rep(i,n)
+    {
+        if(l <= a[i] && a[i] <= r) cout << a[i] << ' ';
+        else
+        {
+            int tmp_1 = abs(l - a[i]);
+            int tmp_2 = abs(r - a[i]);
+            if(tmp_1 < tmp_2) cout << l << ' ';
+            else cout << r << ' ';
+        }
+    }
+    return 0;
+}
