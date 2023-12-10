13266430831817225a940d796e4f9617bc30af66
haru210 <harun20070320@gmail.com>
2023-12-10 22:54:56 +0900

    やるだけ

---
+++ ABC332/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+int main()
+{
+    int n,s,k;
+    cin >> n >> s >>k;
+    ll sum = 0;
+    rep(i,n)
+    {
+        int p,q;
+        cin >> p >> q;
+        sum += (p * q);
+    }
+    if(sum < s) sum += k;
+    cout << sum << endl;
+    return 0;
+}
