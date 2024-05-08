4bdb8ac0dc7945a81aa21285b570dcb548cbba42
haru210 <miroku.pr@outlook.jp>
2024-05-08 19:42:57 +0900

    肩の長さを合計して、一番上を全パターン試す

---
+++ ABC352/C.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n], b[n];
+    rep(i,n) cin >> a[i] >> b[i];
+    ll ssum = 0;
+    rep(i,n)
+    {
+        ssum += a[i];
+    }
+    ll ans = 0;
+    rep(i, n)
+    {
+        ll tmp = ssum - a[i] + b[i];
+        ans = max(tmp, ans);
+    }
+    cout << ans << endl;
+    return 0;
+}
