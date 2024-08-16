0723b47636de726bc816dbfd148c2fd1804c71f6
haru210 <miroku.pr@outlook.jp>
2024-08-16 15:08:06 +0900

    a - b + 1

---
+++ ABC209/A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    int c = b - a;
+    c++;
+    if(c <= 0) cout << 0 << endl;
+    else cout << c << endl;
+    return 0;
+}
