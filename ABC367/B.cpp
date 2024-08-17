4b9c52ef36ce799da563efdcabac207b33b73160
haru210 <miroku.pr@outlook.jp>
2024-08-17 23:17:53 +0900

    末尾から見る

---
+++ ABC367/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+
+int main()
+{
+    string s;
+    cin >> s;
+    int last = s.size() - 1;
+    while(true)
+    {
+        if(s[last] == '0')
+        {
+            last--;
+        }
+        else if(s[last] == '.')
+        {
+            last--;
+            break;
+        }
+        else break;
+    }
+    rep(i, last + 1) cout << s[i];
+    cout << endl;
+    return 0;
+}
