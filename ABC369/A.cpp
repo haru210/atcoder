b84b2b3b3fba9315ece106b59a46296814038b5a
haru210 <miroku.pr@outlook.jp>
2024-09-03 21:51:33 +0900

    差が0なら1つ、それ以外なら差の偶奇で

---
+++ ABC369/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if(a == b) cout << 1 << endl;
+    else if(abs(a - b) % 2 == 0) cout << 3 << endl;
+    else cout << 2 << endl;
+    return 0;
+}
