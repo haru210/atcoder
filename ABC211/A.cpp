b0bd1eedd945333d969b4efb365eccbd78ec4281
haru210 <miroku.pr@outlook.jp>
2025-03-29 20:55:09 +0900

    doubleを使う

---
+++ ABC211/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    long double c = (long double)(a - b) / 3 + b;
+    cout << fixed << setprecision(10) << c << endl;
+    return 0;
+}
