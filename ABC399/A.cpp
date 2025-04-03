74187a0aa1ae87ff58c599046e6c93306c6c2637
haru210 <miroku.pr@outlook.jp>
2025-04-03 20:38:06 +0900

    やる

---
+++ ABC399/A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+
+int main()
+{
+    int n;
+    string s, t;
+    cin >> n >> s >> t;
+    int ans = 0;
+    rep(i, n)
+    {
+        if(s[i] != t[i]) ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
