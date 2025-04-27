5e61de91ae0938db1e0e5b032ab8f9209a16fd9c
haru210 <miroku.pr@outlook.jp>
2025-04-27 23:21:03 +0900

    ?をワイルドカードとしてストリングマッチング

---
+++ ABC403/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+
+int main()
+{
+    string t;
+    string u;
+    cin >> t >> u;
+    bool ans = false;
+    rep(i, t.size() - (u.size() - 1))
+    {
+        bool ok = true;
+        rep(j, u.size())
+        {
+            if(!(t[i + j] == '?' || t[i + j] == u[j])) ok = false;
+        }
+        if(ok)
+        {
+            ans = true;
+            break;
+        }
+    }
+    YesNo(ans);
+    return 0;
+}
