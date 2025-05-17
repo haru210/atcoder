785eaef68995d564aa037bce027e87f83ee4f434
haru210 <miroku.pr@outlook.jp>
2025-05-17 23:09:59 +0900

    頑張る

---
+++ ABC406/A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    bool ok = false;
+    if(c < a) ok = true;
+    else if(c == a && d < b) ok = true;
+    YesNo(ok);
+    return 0;
+}
