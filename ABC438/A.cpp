d3c7c52347f0e1a4c5b943598106af8b6b9ebec0
haru210 <miroku.pr@outlook.jp>
2025-12-27 22:47:25 +0900

    1年で何日ずれるか計算し、dから引く

---
+++ ABC438/A.cpp
@@ -0,0 +1,24 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl
+
+const int inf = 2147483647;
+const long long l_inf = 9223372036854775807;
+
+int main()
+{
+    int d, f;
+    cin >> d >> f;
+    int tmp = d % 7;
+    int ans = ((f - tmp) + 7) % 7;
+    if (ans == 0) ans = 7;
+    cout << ans << endl;
+    return 0;
+}
